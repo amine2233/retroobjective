c462172538f9dababf53a121eb0ffe7b6d271c0c
Bensalah <amine.bensalah@intech-consulting.fr>
2020-01-21 08:00:03 +0100

    first commit

---
+++ Sources/RetroObjective/RetroObjective.h
@@ -0,0 +1,18 @@
+//
+//  RetroObjective.h
+//  RetroObjective
+//
+//  Created by Amine Bensalah on 21/01/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RetroObjective.
+FOUNDATION_EXPORT double RetroObjective_VersionNumber;
+
+//! Project version string for RetroObjective.
+FOUNDATION_EXPORT const unsigned char RetroObjective_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RetroObjective/PublicHeader.h>
+
+
